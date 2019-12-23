0364aeb59c5ff3031c08fcbba850d6f8d58169a8
Vishesh Handa <me@vhanda.in>
2019-12-23 19:35:54 +0100

    Check if in debug mode via Flutter
    Instead of checking via Android / iOS as well.

--- lib/should_enable_analytics.dart
+++ lib/should_enable_analytics.dart
@@ -1,8 +1,16 @@
 import 'dart:async';
 import 'package:flutter/services.dart';
+import 'package:flutter/foundation.dart' as Foundation;
 
 const _channel = const MethodChannel('io.gitjournal/should_enable_analytics');
 
 Future<bool> shouldEnableAnalytics() async {
+  if (isInDebugMode()) {
+    return false;
+  }
   return await _channel.invokeMethod('shouldEnableAnalytics');
+}
+
+bool isInDebugMode() {
+  return Foundation.kDebugMode;
 }
