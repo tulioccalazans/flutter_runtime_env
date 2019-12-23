264ebc5147e87f10889cb67dc71fd6650ff29499
Vishesh Handa <me@vhanda.in>
2019-12-23 12:56:15 +0100

    Initial commit

---
+++ ios/Classes/ShouldEnableAnalyticsPlugin.m
@@ -0,0 +1,20 @@
+#import "ShouldEnableAnalyticsPlugin.h"
+
+@implementation ShouldEnableAnalyticsPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"should_enable_analytics"
+            binaryMessenger:[registrar messenger]];
+  ShouldEnableAnalyticsPlugin* instance = [[ShouldEnableAnalyticsPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
