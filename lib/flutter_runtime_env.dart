3f13aa535938fdeee512d5ef40bde229c9be2288
Vishesh Handa <me@vhanda.in>
2020-05-31 08:43:59 +0200

    Only works on Android

--- lib/flutter_runtime_env.dart
+++ lib/flutter_runtime_env.dart
@@ -1,10 +1,14 @@
 import 'dart:async';
+import 'dart:io';
 import 'package:flutter/services.dart';
 import 'package:flutter/foundation.dart' as Foundation;
 
 const _channel = const MethodChannel('io.gitjournal/flutter_runtime_env');
 
 Future<bool> inFirebaseTestLab() async {
+  if (!Platform.isAndroid) {
+    return false;
+  }
   try {
     var result = await _channel.invokeMethod('inFirebaseTestLab');
     return result;
